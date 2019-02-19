374a4f520a6830cb7e121fef7fbd089d1f0d7c7c
程健锋 <chengjf@kamfu.com.cn>
2019-02-19 11:32:18 +0800

    ok

---
+++ src/test2/test2.java
@@ -0,0 +1,23 @@
+package test2;
+
+public class test2 {
+
+	public void test21() {
+		System.out.println("hhh");
+	}
+	public void test22() {
+		System.out.println("aaa");
+	}
+	public void test23() {
+		System.out.println("ccc");
+	}
+	public void test24() {
+		System.out.println("qqq");
+	}
+	public void tes25t() {
+		System.out.println("ddd");
+	}
+	public void test26() {
+		System.out.println("ggg");
+	}
+}
---
+++ src/test3/test3.java
@@ -0,0 +1,26 @@
+package test3;
+
+public class test3 {
+
+	public String test31() {
+		return "111";
+	}
+	public String test32() {
+		return "222";
+	}
+	public String test33() {
+		return "333";
+	}
+	public String test34() {
+		return "444";
+	}
+	public String test35() {
+		return "555";
+	}
+	public String test36() {
+		return "666";
+	}
+	public String test37() {
+		return "777";
+	}
+}
